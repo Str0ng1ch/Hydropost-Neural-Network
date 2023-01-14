c12f805cbab17dbd83ffe35ba367acd236879620
illler <0250901@gmail.com>
2023-01-14 13:56:19 +0300

    denormalize change

--- src/Normalization.java
+++ src/Normalization.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Normalization {
 
     public float[][] inputs, normalized_inputs, denormalized_inputs;
+    List<double[]> minMax = new ArrayList<>();
 
     public Normalization(float[][] inputs) {
         this.inputs = inputs;
@@ -14,8 +17,11 @@ public class Normalization {
         float[][] tmp = new Matrix(inputs).transpose();
         System.out.println("first: " + Arrays.deepToString(tmp));
         for (int i = 0; i < tmp.length; i++) {
+            double[] minMaxArray = new double[2];
             float max_value = new Matrix(tmp[i]).find_max_in_array();
             float min_value = new Matrix(tmp[i]).find_min_in_array();
+            minMaxArray[0] =  max_value; minMaxArray[1] = min_value;
+            minMax.add(minMaxArray);
             for (int j = 0; j < tmp[i].length; j++)
                 tmp[i][j] = (tmp[i][j] - min_value)/ (max_value - min_value);
         }
@@ -25,8 +31,15 @@ public class Normalization {
 
     public float[][] denormalize() {
         // TODO
-//        текущее * (максимальное - минимальное) + минимальное
-        return inputs;
+        float[][] tmp = new Matrix(inputs).transpose();
+        for (int i = 0; i < tmp.length; i++) {
+            float max_value = (float) minMax.get(i)[0];
+            float min_value = (float) minMax.get(i)[1];
+            for (int j = 0; j < tmp[i].length; j++)
+                tmp[i][j] = tmp[i][j] * (max_value-min_value) + min_value;
+        }
+        System.out.println("second: " + Arrays.deepToString(tmp));
+        return new Matrix(tmp).transpose();
     }
 
 }
